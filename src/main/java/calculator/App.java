b70f5cbc641dfe85570d49cdcfa7656c9b717f31
Kim Hani <32191261@dankook.ac.kr>
2025-01-02 13:35:43 +0900

    4차 커밋 / App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정 / 계산은 Calculator에서 수행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,11 +3,12 @@ package calculator;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double total =0;
+        Calculator calculator = new Calculator();
 
-        while(true){
+        while(true) {
             System.out.print("첫 번째 정수를 입력하세요 : ");
             int n1 = sc.nextInt();
             sc.nextLine();
@@ -19,34 +20,8 @@ public class App {
             int n2 = sc.nextInt();
             sc.nextLine();
 
+            calculator.calculate(n1,operator,n2);
 
-            switch (operator){
-                case("+"):
-                    total = n1 + n2;
-                    System.out.println("더하기 결과 값은 " + total +"입니다.");
-                    break;
-                case("-"):
-                    total = n1 - n2;
-                    System.out.println("빼기 결과 값은 " + total +"입니다.");
-                    break;
-
-                case("*"):
-                    total = n1 * n2;
-                    System.out.println("곱하기 결과 값은 " + total +"입니다.");
-                    break;
-
-                case("/"):
-                    if(n2==0){
-                        System.out.println("0으로는 나눌 수 없습니다.");
-                        return;
-                    }
-                    total = (double) n1 / n2;
-                    System.out.println("나누기 결과 값은 " + total +"입니다.");
-                    break;
-                default:
-                    System.out.println("정확한 연산기호를 입력하세요");
-                    break;
-            }
             System.out.println("계속 하시겠습니까? (exit 입력 시 종료)");
             if(sc.nextLine().equals("exit")) break;
         }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,34 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calculator {
+    private Queue<Double> result = new LinkedList<>();
+
+    public double getQueue(){
+        return result.poll();
+    }
+
+    public double calculate(int n1,String operator,int n2) {
+        double total=0;
+        switch (operator) {
+            case ("+"):
+                total = n1+n2;
+                break;
+            case ("-"):
+                total = n1-n2;
+                break;
+            case ("*"):
+                total = n1*n2;
+                break;
+            case ("/"):
+                total = (double)n1/n2;
+                break;
+        }
+        result.add(total);
+        return total;
+    }
+
+
 }
