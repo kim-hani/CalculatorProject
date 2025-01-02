aa0ab0a7b271567c05aa0ff709bcbeda81e528e7
Kim Hani <32191261@dankook.ac.kr>
2025-01-02 14:37:53 +0900

    5차 커밋 / Lv1 : calculator1 , Lv2 : calculator2

--- src/main/java/calculator2/App.java
+++ src/main/java/calculator2/App.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator2;
 
 import java.util.Scanner;
 
@@ -22,8 +22,14 @@ public class App {
 
             calculator.calculate(n1,operator,n2);
 
-            System.out.println("계속 하시겠습니까? (exit 입력 시 종료)");
+            System.out.println("메뉴 선택");
+            System.out.println("계속 진행 : continue 입력  / " +
+                    "가장 처음으로 저장된 결과 확인 및 삭제 : poll 입력 / " +
+                    "모든 결과 삭제 : clear 입력 / " +
+                    "종료 : exit 입력" );
             if(sc.nextLine().equals("exit")) break;
+            else if(sc.nextLine().equals("poll")) calculator.getQueue();
+            else if (sc.nextLine().equals("clear")) calculator.clearQueue();
         }
     }
 }
--- src/main/java/calculator2/Calculator.java
+++ src/main/java/calculator2/Calculator.java
@@ -1,4 +1,4 @@
-package calculator;
+package calculator2;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,8 +6,18 @@ import java.util.Queue;
 public class Calculator {
     private Queue<Double> result = new LinkedList<>();
 
+    // getter
     public double getQueue(){
+        if(result.isEmpty()){
+            throw new IllegalStateException("저장된 결과값이 없습니다.연산을 먼저 수행하세요.");
+        }
         return result.poll();
+    }
+
+    // setter
+    public void clearQueue(){
+        System.out.println("모든 결과 삭제");
+        result.clear();
     }
 
     public double calculate(int n1,String operator,int n2) {
